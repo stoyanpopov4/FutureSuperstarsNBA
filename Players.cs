d04d8acd5e47adf83f392daf304705b47c1edc98
stoqnstr <stoqn.popov4@abv.bg>
2019-07-01 14:45:34 +0300

    first commit

---
+++ Players.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutureSuperstarsNBA
+{
+    class Players
+    {
+        public string Name { get; set; }
+        public int PlayingSince { get; set; }
+        public string Position { get; set; }
+        public int Rating { get; set; }
+
+        public String ToCSV()
+        {
+            return string.Format(Name + ", " + Rating.ToString());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace FutureSuperstarsNBA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxYears, minRating, currentYear, currentYearsPlayed;
+            bool isQualified = false;
+
+            DateTime current = DateTime.Now;
+            currentYear = current.Year;
+
+            string jsonPath = @"C:\Users\stoqn\source\repos\FutureSuperstarsNBA\FutureSuperstarsNBA\Players.json";
+            string csvPath = @"C:\Users\stoqn\source\repos\FutureSuperstarsNBA\FutureSuperstarsNBA\QualifiedPlayers.txt";
+
+            string json = File.ReadAllText(jsonPath);
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            var playerList = ser.Deserialize<List<Players>>(json);
+
+            Console.WriteLine("Please enter the maximum years played in the league and the minimum rating that is required from players to qualify.\n ");
+
+            if(int.TryParse(Console.ReadLine(), out maxYears) && int.TryParse(Console.ReadLine(), out minRating))
+            {
+                using (StreamWriter file = new StreamWriter(csvPath))
+                {
+                    file.WriteLine("Name" + "," + "Rating");
+                    foreach (var player in playerList)
+                    {
+                        currentYearsPlayed = currentYear - player.PlayingSince;
+                        if (currentYearsPlayed <= maxYears && player.Rating >= minRating)
+                        {
+                            isQualified = true;
+                            file.WriteLine(player.ToCSV());   
+                        }
+                    }
+
+                    if(isQualified == true)
+                        Console.WriteLine("\nSucces. The players that qualified are recorded correct.");
+                    else
+                        Console.WriteLine("\nThere isnt any players ho match the requirements.");
+                }
+            }
+            else
+                Console.WriteLine("You have entered incorrect data.");
+        }
+    }
+}
